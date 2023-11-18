ee2e31cf38a101008419593b4d4c76ddb38bc5c9
Arjun <34741958+Arjun-G-04@users.noreply.github.com>
2023-11-18 21:34:24 +0530

    feat: basic player movement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,34 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet';
+
+function Pin() {
+    const map = useMapEvent('move', () => {
+        setCoords([map.getCenter().lat, map.getCenter().lng]);
+    })
+
+    const [coords, setCoords] = useState([map.getCenter().lat, map.getCenter().lng]);
+    
+    return (
+        <Marker position={coords}>
+            <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+    )
+}
 
 export default function Home() {
+
     return (
-        <div className="h-screen w-screen bg-black text-white pt-10">
+        <div className="h-screen w-screen bg-black text-white flex flex-col items-center justify-evenly">
             <div className="text-3xl text-center">Leaflet PoC</div>
-            <div className='h-[50%]'>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className=' h-60'>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className=' h-[90%] aspect-square'>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                <Pin />
             </MapContainer>
-            </div>
         </div>
     );
 }
